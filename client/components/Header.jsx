c4515a753453440e3eb97153f97b6cf72f72c829
mktsewhoy <mktsewhoy@hotmail.co.nz>
2019-04-26 09:53:43 +1200

    Merge with /dev branch.

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
       <h1 className='logofont'>
-        {/* <span className='fa fa-beer' /> */}
         WHAT&iquest;LY
-        {/* <span className='fa fa-beer' /> */}
       </h1>
     )
   }
