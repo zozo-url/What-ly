e524846073f13ca770460b2b3cec944ec0b82f93
mktsewhoy <mktsewhoy@hotmail.co.nz>
2019-04-26 15:04:44 +1200

    Change slogan as per request.

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <span className='logofont'>WHAT</span>
         <span className='logoquestion'>&iquest;</span>
         <span className='logofont'>LY</span>
-        <span className='logotag'>A picture is worth a thousand words</span>
+        <span className='logotag'>A picture is worth a thou</span>
       </div>
     )
   }
